8c9eacb520dd188ca4c4598d1bf6f72f85eb2742
VinzOo93 <v.orru@live.fr>
2023-12-17 18:52:41 +0100

    scale to symfony 6.4 add debug tools ok

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,6 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
