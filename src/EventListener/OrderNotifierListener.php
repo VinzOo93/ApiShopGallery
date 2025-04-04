72f90bd4820d5f95045c766cfd0e00355ff0b983
VinzOo93 <v.orru@live.fr>
2025-04-05 00:39:23 +0200

    add email confirmation template and set credantials

--- src/EventListener/OrderNotifierListener.php
+++ src/EventListener/OrderNotifierListener.php
@@ -6,14 +6,20 @@ use App\Entity\Order;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Events;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Symfony\Component\Mime\Address;
 
 #[AsEntityListener(event: Events::postPersist, method: 'postPersist', entity: Order::class)]
 readonly class OrderNotifierListener
 {
-    public function __construct(private MailerInterface $mailer)
+    public function __construct(
+        private MailerInterface $mailer,
+        private ParameterBagInterface $parameterBag
+    )
     {
     }
 
@@ -22,13 +28,23 @@ readonly class OrderNotifierListener
      */
     public function postPersist(Order $order, PostPersistEventArgs $eventArgs): void
     {
-        $email = (new Email())
-            ->from('v.orru@live.fr')
-            ->to('you@example.com')
-            ->subject('Time for Symfony Mailer!')
-            ->text('Sending emails is fun again!')
-            ->html('<p>See Twig integration for better HTML integration!</p>');
-
+        $email = (new TemplatedEmail())
+            ->from(new Address($this->parameterBag->get('app.api.email_from'), 'Vincent ORRU'))
+            ->to($order->getPayment()->getEmail())
+            ->subject('Votre commande est enregistrée')
+            ->htmlTemplate('email/confirmation-order.html.twig')
+            ->context([
+                'firstname' => $order->getPayment()->getFirstname(),
+                'name' => $order->getPayment()->getName(),
+                'items' => $order->getCart()->getItems(),
+                'address' => $order->getPayment()->getAddress(),
+                'city' => $order->getPayment()->getCity(),
+                'postalCode' => $order->getPayment()->getPostalCode(),
+                'country' => $order->getPayment()->getCountry(),
+                'shipping' => $order->getCart()->getShipping(),
+                'taxes' => $order->getCart()->getTaxes(),
+                'total' => $order->getCart()->getTotal()
+            ]);
         $this->mailer->send($email);
     }
 }
